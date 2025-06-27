94577c76620f482f80e3e13b7177b925a9046baf
Jaydev Rajbanshi <jayrajbanshi234@gmail.com>
2025-06-27 19:48:01 +0545

    adding

---
+++ calculatorrr/Program.cs
@@ -0,0 +1,62 @@
+﻿
+namespace ConsoleApp3;
+
+public class program()
+{
+    public static void Main()
+    {
+        {
+            var obj = new calculator();
+            int a;
+            do
+            {
+                double total, temp;
+
+
+                Console.WriteLine("Enter the first value:");
+                obj.a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the second value:");
+                obj.b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(" \n1. Addition \n2. Subtraction \n3. Multiplication \n4. Division ");
+                temp = int.Parse(Console.ReadLine());
+
+                switch (temp)
+                {
+                    case 1:
+                        {
+                            total = obj.add();
+                            break;
+                        }
+                    case 2:
+                        {
+                            total = obj.sub();
+                            break;
+                        }
+                    case 3:
+                        {
+                            total = obj.mul();
+                            break;
+                        }
+                    case 4:
+                        {
+                            total = obj.div();
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("Invalid operation.");
+                            return;
+                        }
+                }
+                Console.WriteLine($"The result is :{total}");
+                Console.WriteLine("Continue Y/n: \n 1. yes \n 2. no");
+                a = int.Parse(Console.ReadLine());
+            }
+
+            while (a == 1);
+        }
+    }
+}
---
+++ calculatorrr/obj.cs
@@ -0,0 +1,30 @@
+﻿
+namespace ConsoleApp3;
+
+public class calculator
+{
+    public double a, b, c;
+
+    public double add()
+    {
+        c = a + b;
+        return c;
+    }
+    public double sub()
+    {
+        c = a - b;
+        return c;
+    }
+    public double mul()
+    {
+        c = a * b;
+        return c;
+    }
+    public double div()
+    {
+        c = a * b;
+        return c;
+    }
+
+}
+
